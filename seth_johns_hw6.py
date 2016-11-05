3236faffdc3779f66db8add6f3b15892215d1411
Seth Johns <sethjohns@mail.weber.edu>
2016-11-05 15:27:32 -0600

    made an array of all errors

--- seth_johns_hw6.py
+++ seth_johns_hw6.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 import sys
 import urllib.request
+import re
+import pprint
 
 def readFile(url):
     webText = urllib.request.urlopen(url)
     text = webText.read()
-    print(text)
+    pattern = '\/[0-9a-zA-Z]*\/[0-9a-zA-Z]*\/[0-9a-zA-Z]*\/[0-9a-zA-Z]*'
+    text = text.replace(b'\n',b' ')
+    bitStringArray = text.split()
+    stringArray = []
+    for index in bitStringArray:
+        stringArray.append(index.decode('utf-8'))
+    narrowSearch = []
+    for index in stringArray:
+        if re.match(pattern, index):
+            narrowSearch.append(index)
+    dictResult = dict()
+    for index in narrowSearch:
+        if dictResult.get(index) == None:
+            dictResult = dictResult + {index:0}
+        else: 
+            dictResult[index] += 1
+    print(dictResult)
+
 
 # Main Function
 def main():
     """
     Tests readFile
     """
-    url = 'http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test'
-    #url = 'http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.full'
+    #url = 'http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test'
+    url = 'http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.full'
     readFile(url)
     return
 
